32314846ff7fc87187936c91618069718e47868b
minato37103710 <arigatoudane@outlook.jp>
2022-05-05 13:52:59 +0900

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Tiny-URL-Wrapper",
+    name="TinyURLWrapper",
     version="0.1.0",
     author="orarange",
     author_email="arigatoudane@outlook.jp",
