c8d7d3c2537542bae1513eef9319b730d9479669
minato37103710 <arigatoudane@outlook.jp>
2022-05-05 16:04:11 +0900

    バージョン変えるのを忘れてた

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Tiny_URL",
-    version="0.1.1",
+    version="0.1.2",
     author="orarange",
     author_email="arigatoudane@outlook.jp",
     description="this is an tiny url Python wrapper",
